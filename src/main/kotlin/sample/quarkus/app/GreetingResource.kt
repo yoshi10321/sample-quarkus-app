ab3a1e26b18c756902923bf0338ba43cd4e592f6
yoshi10321 <pooh.aflac@gmail.com>
2019-07-19 10:52:21 +0900

    first commit

---
+++ src/main/kotlin/sample/quarkus/app/GreetingResource.kt
@@ -0,0 +1,15 @@
+package sample.quarkus.app
+
+import javax.ws.rs.GET
+import javax.ws.rs.Path
+import javax.ws.rs.Produces
+import javax.ws.rs.core.MediaType
+
+@Path("/greeting")
+class GreetingResource {
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    fun hello() = "hello world"
+
+}
